e110b72963e016e6f30dee1eb089579edac02323
Ben Blum <bblum@andrew.cmu.edu>
2017-03-13 14:06:14 -0400

    itg - count xover footswitches by remembering the last jack arrow

--- itg/code/ITG.hs
+++ itg/code/ITG.hs
@@ -4,8 +4,9 @@ import Data.Maybe
 
 data Step = L | D | U | R | Jump deriving Eq
 
-data AnalysisState = S { steps :: Int, xovers :: Int, switches :: Int, jacks :: Int,
-                         doubles :: Int, lastStep :: Maybe Step, lastJack :: Bool,
+data AnalysisState = S { steps :: Int, xovers :: Int, switches :: Int,
+                         jacks :: Int, doubles :: Int, xoverfs :: Int,
+                         lastStep :: Maybe Step, lastJack :: Maybe Step,
                          lastFlip :: Bool, lastFoot :: Bool, stepsLR :: [Bool] }
 
 commitStream :: AnalysisState -> AnalysisState
@@ -27,11 +28,14 @@ commitStream s = maybe s0 splitStream splitIndex
           splitStream i = s2 { doubles = doubles s2 + 1 }
               where (steps1, steps2) = splitAt i $ stepsLR s
                     s1 = commitStream s  { stepsLR = steps1 }
-                    s2 = commitStream s1 { stepsLR = steps2, lastJack = False }
+                    s2 = commitStream s1 { stepsLR = steps2, lastJack = Nothing }
           -- normal case; consider the whole stream at once
+          jack = lastJack s /= Nothing
+          jackLR = lastJack s == Just L || lastJack s == Just R
           s0 = s { xovers   = xovers   s + if f then ns - nx else nx,
-                   switches = switches s + fromEnum (f == lastFlip s && lastJack s),
-                   jacks    = jacks    s + fromEnum (f /= lastFlip s && lastJack s),
+                   switches = switches s + fromEnum (f == lastFlip s && jack),
+                   jacks    = jacks    s + fromEnum (f /= lastFlip s && jack),
+                   xoverfs  = xoverfs  s + fromEnum (f == lastFlip s && jackLR),
                    lastFlip = f, stepsLR = [] }
 
 analyzeStep :: AnalysisState -> Step -> AnalysisState
@@ -39,12 +43,12 @@ analyzeStep s step
     -- a jump resets the footing, so the next step can be stepped with either
     -- foot. commit the stream so far to treat it separately from what follows.
     -- bracket-jumps are, of course, future work.
-    | step == Jump = (commitStream s) { lastStep = Nothing, lastJack = False }
+    | step == Jump = (commitStream s) { lastStep = Nothing, lastJack = Nothing }
     -- two steps on the same arrow might be a jack, or might be a footswitch.
     -- to figure out which, commit the stream so far, and begin a new stream
     -- whose footing will retroactively determine how to foot this step.
     -- also, unlike jumps, this step gets counted as part of the next stream.
-    | lastStep s == Just step = stream (commitStream s) { lastJack = True }
+    | lastStep s == Just step = stream (commitStream s) { lastJack = Just step }
     -- a normal streamy step.
     | otherwise = stream s
     where foot = not $ lastFoot s
@@ -56,7 +60,7 @@ analyzeStep s step
                          stepsLR = addStep foot step $ stepsLR s }
 
 analyze :: [Step] -> AnalysisState
-analyze = commitStream . foldl analyzeStep (S 0 0 0 0 0 Nothing False False False [])
+analyze = commitStream . foldl analyzeStep (S 0 0 0 0 0 0 Nothing Nothing False False [])
 
 -- turns a line of stepchart, eg "0100", into a Step, e.g. "D"
 -- in stepchart-ese: 1 = tap; 2 = hold; 3 = hold release; 4 = roll; M = mine
@@ -73,7 +77,8 @@ process (title,(name:diff:feet:_:rest)) = intercalate "\t" $ metadata ++ result
           trim = reverse . tail . dropWhile isSpace . reverse . dropWhile isSpace
           metadata = map trim [title,name,diff,feet]
           finalState = analyze $ catMaybes $ map (stepify . dropWhile isSpace) rest
-          result = map (show . ($ finalState)) [steps, xovers, switches, jacks, doubles]
+          result = map (show . ($ finalState)) [steps, xovers, switches,
+                                                jacks, doubles, xoverfs]
 
 -- splits up the input file by the "#NOTES:" lines
 charts :: String -> [String] -> [(String, [String])]
