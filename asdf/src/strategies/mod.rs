3c81a978071582a9194cd85e1f59c55b43999dc1
ben blum <bblum0@gmail.com>
2020-03-22 16:35:44 -0400

    random less cute

--- asdf/src/strategies/mod.rs
+++ asdf/src/strategies/mod.rs
@@ -6,3 +6,4 @@ mod random;
 pub use cdfbisect::CdfBisect;
 pub use linear::LinearSearch;
 pub use naive::NaiveBinarySearch;
+pub use random::{ChooseRandomly, How};
--- asdf/src/strategies/random.rs
+++ asdf/src/strategies/random.rs
@@ -2,48 +2,41 @@ use rand::Rng;
 
 use crate::sim::*;
 
-pub trait SelectRandom {
-    fn select_random(s: &SimulationState, up_to: usize) -> usize;
-}
+#[cfg_attr(not(test), allow(unused))]
+pub enum How { Uniformly, WeightedByCDF }
 
-// aggressively avoiding code reuse
-pub enum Uniformly {}
-pub enum WeightedByCDF {}
-
-pub struct ChooseRandomly<T> {
+pub struct ChooseRandomly {
     earliest_known_buggy_commit: usize,
-    _how: std::marker::PhantomData<T>,
+    how: How,
 }
 
-impl<T> ChooseRandomly<T> {
+impl ChooseRandomly {
     #[cfg_attr(not(test), allow(unused))]
-    pub fn new(s: &SimulationState) -> Self {
+    pub fn new(s: &SimulationState, how: How) -> Self {
         Self {
             earliest_known_buggy_commit: s.pdf.len() - 1,
-            _how: std::marker::PhantomData,
+            how,
         }
     }
 }
 
-impl SelectRandom for Uniformly {
-    fn select_random(_: &SimulationState, up_to: usize) -> usize {
-        rand::thread_rng().gen_range(0, up_to)
-    }
-}
-
-impl SelectRandom for WeightedByCDF {
-    fn select_random(s: &SimulationState, up_to: usize) -> usize {
-        let p = rand::thread_rng().gen_range(0.0, s.cdf.last().expect("empty cdf"));
-        let result = s.last_cdf_index_not_exceeding(p);
-        assert!(result < up_to, "would test known buggy commit");
-        result
-    }
-}
-
-impl<T: SelectRandom> BisectStrategy for ChooseRandomly<T> {
+impl BisectStrategy for ChooseRandomly {
     fn select_commit(&mut self, s: &SimulationState) -> usize {
         assert_ne!(self.earliest_known_buggy_commit, 0, "bug already found");
-        <T as SelectRandom>::select_random(s, self.earliest_known_buggy_commit)
+        match self.how {
+            How::Uniformly => {
+                rand::thread_rng().gen_range(0, self.earliest_known_buggy_commit)
+            },
+            How::WeightedByCDF => {
+                // don't just say 1.0, and get got by floating point
+                let max_cdf = s.cdf.last().expect("empty cdf");
+
+                let p = rand::thread_rng().gen_range(0.0, max_cdf);
+                let result = s.last_cdf_index_not_exceeding(p);
+                assert!(result < self.earliest_known_buggy_commit);
+                result
+            },
+        }
     }
 
     fn notify_result(&mut self, result: BisectAttempt) {
