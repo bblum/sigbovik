bb14be85a5b831b9adfb7ba23d24b42371296f95
ben blum <bblum0@gmail.com>
2021-01-10 16:55:00 -0500

    random lua change

--- itg/code/itg.lua
+++ itg/code/itg.lua
@@ -21,7 +21,7 @@ function parse_sm(sm_contents)
 	while i <= #sm_contents do
 		local line = sm_contents[i]
 		if line:find("#TITLE:") then
-			title = line:gsub(".*#TITLE:(.*);$", "%1")
+			title = line:gsub(".*#TITLE:(.*);\r?$", "%1")
 			break
 		end
 		i = i + 1
@@ -67,3 +67,95 @@ end
 lines = read_input(arg[1])
 title, charts = parse_sm(lines)
 print(title .. " has " .. #charts .. " charts!")
+
+-- "Prelude"
+function append(list1, list2) -- note: in-place append. O(length(list2)) (i hope)
+	for _,val in pairs(list2) do
+		table.insert(list1, val)
+	end
+end
+
+function takeWhile(list, f)
+	result = {}
+	for _,val in pairs(list) do
+		if (f(val)) then
+			table.insert(result, val)
+		else
+			break
+		end
+	end
+	return result
+end
+
+function dropWhile(list, f)
+	result = {}
+	ready = false
+	for _,val in pairs(list) do
+		if not ready and not f(val) then
+			ready = true
+		end
+		if ready then
+			table.insert(result, val)
+		end
+	end
+	return result
+end
+
+function dropWhileIncluding(list, f) -- as dropWhile but loses the 1st matching element
+	result = {}
+	ready = false
+	for _,val in pairs(list) do
+		if not ready then
+			if not f(val) then
+				ready = true
+			end
+		elseif ready then
+			table.insert(result, val)
+		end
+	end
+	return result
+end
+
+function map(list, f)
+	result = {}
+	for _,val in pairs(list) do
+		table.insert(result, f(val))
+	end
+	return result
+end
+
+function filter(list, f)
+	result = {}
+	for _,val in pairs(list) do
+		if f(val) then
+			table.insert(result, val)
+		end
+	end
+	return result
+end
+
+function findIndex(list, f)
+	for i,val in pairs(list) do
+		if (f(val)) then
+			return i
+		end
+	end
+	return nil
+end
+
+-- charts :: String -> [String] -> [(String, [String])]
+function charts(title, contents)
+	
+end
+
+-- note: inserting 'nil' into a table is a noop; it doesn't even allocate the index
+test = filter({10, 20, 30, 40, 30, 20, 10}, function(x) return x>25 end)
+for i,val in pairs(test) do
+	print("test: " .. i .. ", " .. tostring(val))
+end
+test2 = findIndex({10, 20, 30, 40, 30, 20, 10}, function(x) return x>55 end)
+if test2 == nil then
+	print("crap")
+else
+	print(test2)
+end
